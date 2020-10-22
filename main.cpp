9f5cf8301e9aa12df6b59534b4d555ef5f2342de
lauravidal15@hotmail.com <lauravidal15@hotmail.com>
2020-10-22 09:46:02 -0500

    imprimo numeros

--- main.cpp
+++ main.cpp
@@ -14,7 +14,10 @@ int main()
 {
     char a[TAM]="AB11CD44";
     for(int var=0;var<TAM; ++var) cout<<a[var]<<endl;
-
+     cout<< " "<< endl;
+    for(int var=0;var<TAM; ++var){
+       if(a[var]>='0' && a[var]<='9') cout<<a[var];
+    }
 
 
     cout << "Hello World!" << endl;
