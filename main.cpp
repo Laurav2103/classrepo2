8211270d4094ae8872bd7296c73470d659e67252
lauravidal15@hotmail.com <lauravidal15@hotmail.com>
2020-10-22 08:57:05 -0500

    inicializacion variable que almacena la cadena de entrada

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
+/*
+ */
+
+
+
 #include <iostream>
 
+//Variables globales y macros
+
+#define TAM (int)10
 using namespace std;
 
 int main()
 {
+    char a[TAM]="AB11CD44";
+    for(int var=0;var<TAM; ++var) cout<<a[var]<<endl;
+
+
+
     cout << "Hello World!" << endl;
     return 0;
 }
